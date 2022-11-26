85b42a41d68ab9337c583e2c9cc9e2ec63a55e04
RickyRaMx_c149 <ricardo.rmeza@live.com>
2022-11-26 01:06:33 -0600

    🚑 config next

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['https://dummyimage.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'dummyimage.com',
-        port: '',
-        pathname: '',
-      },
-    ],
-  },
+  
 }
 
 module.exports = nextConfig
