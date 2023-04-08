7bc8601f780f0357bc9bd438e86f0a0b4e1bc261
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-04-08 21:54:58 +0000

    fix error img evaluacion

--- components/Servicios.js
+++ components/Servicios.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import evaluacionImg from '../public/images/evaluacion.jpg'
+
 
 export default function Servicios() {
   return (
