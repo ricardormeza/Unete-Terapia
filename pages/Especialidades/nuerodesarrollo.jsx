0aa81e61f0bcf8b50325b1fcd3f785ff60e5fe2b
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-04-09 22:28:46 +0000

    h3 fix bug

--- pages/Especialidades/nuerodesarrollo.jsx
+++ pages/Especialidades/nuerodesarrollo.jsx
@@ -305,8 +305,9 @@ export default function nuerodesarrollo() {
                     {/* CTA INICIO */}
                 <section className="text-gray-600 body-font bg-green-400 py-3 mt-3">
                     <div className="container mt-5 mb-5">
-                    <h3 className="text-center mt-5 mb-5 font-DMSans text-black">
-                    Te brindare las herramientas valiosas para apoyar a las personas con TDAH y así mejorar su calidad de vida.
+                        <h3 className="text-center mt-5 mb-5 font-DMSans text-black">
+                        Te brindare las herramientas valiosas para apoyar a las personas con TDAH y así mejorar su calidad de vida.
+                        </h3>
                         <div className="grid place-items-center ">
                         <Link href='https://www.doctoralia.com.mx/yeimy-rocio-avila-ponton/psicologo/tuxtla-gutierrez'>
                             <button className="justify-self-center text-white bg-pink-600 border-0 py-2 px-6 focus:outline-none hover:bg-pink-500 rounded text-lg">Agendar cita</button>
