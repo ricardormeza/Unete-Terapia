f95c2046bb0675d001e4f600d39afe0261f9529b
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-05-01 21:04:27 +0000

    carousel change

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -16,7 +16,7 @@ function UncontrolledExample() {
                     placeholder
                 />
                 <Carousel.Caption>
-                    <h3 className='text-green-500'>Neurodesarrollo</h3>
+                    <h3 className='text-green-500'>Desarrollo Neurológico</h3>
                     <p className='text-green-500 font-bold'>Mejoremos la comunicación, las habilidades sociales, el rendimiento académico y autoestima de quien más quieres.</p>
                 </Carousel.Caption>
             </Carousel.Item>
@@ -31,7 +31,7 @@ function UncontrolledExample() {
                 />
 
                 <Carousel.Caption>
-                    <h3 className='text-green-500'>Intervención terapéutica</h3>
+                    <h3 className='text-green-500'>Terapia Individual</h3>
                     <p className='text-green-500 font-bold'>Desarrollar una mayor autoconciencia, lo que te permitirá tomar decisiones más conscientes y saludables en tu vida.</p>
                 </Carousel.Caption>
             </Carousel.Item>
@@ -47,14 +47,14 @@ function UncontrolledExample() {
                 <Carousel.Caption>
                     <h3 className='text-green-500 font-bold'>Talleres de formación</h3>
                     <p className='text-green-500 font-bold'>
-                    Puede ayudarte a encontrar un significado en la muerte y a encontrar consuelo en momentos difíciles.
+                    Mejoraremos la comunicación, el manejo del estrés, la resolución de conflictos, el desarrollo de habilidades sociales.
                     </p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <Image
                     className="d-block w-100"
-                    src="/images/mujer-con-ansiedad.jpeg"
+                    src="/images/orientacion-familiar-portada.jpg"
                     alt="Mujer con ansiedad"
                     width={900}
                     height={900}
@@ -63,7 +63,23 @@ function UncontrolledExample() {
                 <Carousel.Caption>
                     <h3 className='text-green-500 font-bold'>Orientación familiar</h3>
                     <p className='text-green-500 font-bold'>
-                    La terapia puede ayudarte a identificar los factores que contribuyen a tu depresión y a encontrar formas de abordarlos.
+                    Trabajaremos en desarrollar habilidades de comunicación efectiva, resolver conflictos, y mejorar la calidad de las relaciones familiares.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <Image
+                    className="d-block w-100"
+                    src="/images/educacion-inclusiva-1.jpg"
+                    alt="orientacion familiar"
+                    width={900}
+                    height={900}
+                    priority
+                />
+                <Carousel.Caption>
+                    <h3 className='text-green-500 font-bold'>Educación Inclusiva</h3>
+                    <p className='text-green-500 font-bold'>
+                    La educación inclusiva es importante porque proporciona una educación de calidad a todos los estudiantes.
                     </p>
                 </Carousel.Caption>
             </Carousel.Item>
