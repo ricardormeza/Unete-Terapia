e8dbc975a5a8e34e8c1db78af6f73c82230c8ee0
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-06-01 20:43:51 -0700

    test mision-vision

--- components/Mision/VisionMision.js
+++ components/Mision/VisionMision.js
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function VisionMision() {
   return (
-    <div>VisionMision</div>
+    <>
+        <section className="text-gray-600 body-font overflow-hidden">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-wrap -m-12">
+                <div className="p-12 md:w-1/2 flex flex-col items-start">
+                    
+                    <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">Misión</h2>
+                    <p className="leading-relaxed mb-8">Unete es un centro de desarrollo integral de la Salud Mental que aborda la prevencion e intervención  en niños, adolescentes y adultos, asi como  su contexto social y educativo.</p>
+                    <div className="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
+                    
+                    
+                    </div>
+                </div>
+                <div className="p-12 md:w-1/2 flex flex-col items-start">
+                    
+                    <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">Visión</h2>
+                    <p className="leading-relaxed mb-8">Transformar vidas a través de un enfoque innovador e integral en la promoción y la intervención de la salud mental, física y nutricional, guiados por profesionales altamente calificados y con un alto compromiso humano.</p>
+                </div>
+                </div>
+            </div>
+            </section>
+    </>
   )
 }
