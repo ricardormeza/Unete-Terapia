9de211507a2ec46ddb99858ff93ffc747258cfe3
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-06-01 21:04:36 -0700

    value colors

--- components/Mision/VisionMision.js
+++ components/Mision/VisionMision.js
@@ -26,32 +26,32 @@ export default function VisionMision() {
                 <div class="container px-5 py-16 mx-auto">
                     <div class="flex flex-col text-center w-full mb-20">
                     
-                    <h2 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Valores</h2>
+                    <h2 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-green-500">Valores</h2>
                     {/* <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p> */}
                     </div>
                     <div class="flex flex-wrap">
                     <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">DESARROLLO/ INNOVACIÓN:</h2>
+                        <h2 class="text-lg sm:text-xl text-pink-600 font-medium title-font mb-2">DESARROLLO/ INNOVACIÓN:</h2>
                         <p class="leading-relaxed text-base mb-4">A partir de una base científica, desarrollar habilidades para  modificar de manera sustancial las caracteristicas generando nuevos espacios de realidades.</p>
                         
                     </div>
                     <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">COMPROMISO:</h2>
+                        <h2 class="text-lg sm:text-xl text-pink-600 font-medium title-font mb-2">COMPROMISO:</h2>
                         <p class="leading-relaxed text-base mb-4">Con base en la  empatia, desarrollar un alto grado de responsabilidad y pertenencia en el bien estar de los pacientes, generando experiencias que transormen vidas.</p>
                         
                     </div>
                     <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">TRANSPARENCIA:</h2>
+                        <h2 class="text-lg sm:text-xl text-pink-600 font-medium title-font mb-2">TRANSPARENCIA:</h2>
                         <p class="leading-relaxed text-base mb-4">Ética y respeto por la vida misma. Asi cono en lo individual, tambiên el cuidado por el  otro.</p>
                         
                     </div>
                     <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">SOLIDARIDAD:</h2>
+                        <h2 class="text-lg sm:text-xl text-pink-600 font-medium title-font mb-2">SOLIDARIDAD:</h2>
                         <p class="leading-relaxed text-base mb-4">Solidez en que el equipo de trabajo brinde a si mismo la respuesta mss asertiva que impulse e inspire a otros a superarse a si mismo y a la resiliencia.</p>
                         
                     </div>
                     <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">INTEGRIDAD:</h2>
+                        <h2 class="text-lg sm:text-xl text-pink-600 font-medium title-font mb-2">INTEGRIDAD:</h2>
                         <p class="leading-relaxed text-base mb-4">Coherencia entre el ser y el hacer.</p>
                         
                     </div>
