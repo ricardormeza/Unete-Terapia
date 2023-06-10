e95656eaddcbf103635b79a28e56719c5c1bd2bc
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-06-10 22:00:09 +0000

    add adress

--- components/Mapita/index.js
+++ components/Mapita/index.js
@@ -9,7 +9,8 @@ export default function Mymapita() {
 
     return(
     <>
-        <p className="mt-1.5 text-center text-pink-600">Dirección <span className="text-green-600">AV. Noventa Norte Poniente #645 entre Quinta y Sexta Poniente</span></p>
+        <p className="mt-1.5 text-center text-pink-600">Dirección <span className="text-green-600">AV. Noventa Norte Poniente #645 entre Quinta y Sexta Poniente, El Magueyito.
+Barrio Niño de Atocha, Mx 29001</span></p>
         <Mapita />
     </>
     )
