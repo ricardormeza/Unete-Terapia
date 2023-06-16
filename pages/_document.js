3b38013e1a40942ea845304fe1f3d9b8a6142cdf
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-06-16 02:59:36 +0000

    lang es mx

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html>
+        <Html lang="es-MX">
             <Head>
 
 
