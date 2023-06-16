327414a91470ea1d9d9e8a64b09ebb12510d9bae
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-06-16 02:46:21 +0000

    sitemap no implementado

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://bienestar-unete.com/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
