4f2907cf6959f6e74f43c8436a7250975ee78a36
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-07-08 13:05:26 -0700

    preconect a document

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'} />
                 <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:wght@300;400;500;600&family=Source+Sans+Pro:wght@400;600;700&display=swap" rel="stylesheet"/>
-                <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" />
+                <link rel="preconnect" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" />
                 <link rel="icon" href="/images/unete.ico" />
             </Head>
             <body>
