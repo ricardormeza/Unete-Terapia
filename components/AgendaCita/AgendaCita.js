4467deb5be658bf21facb846fd8a285f689ce297
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-10-07 19:53:34 -0700

    add agendarCita desde index

--- components/AgendaCita/AgendaCita.js
+++ components/AgendaCita/AgendaCita.js
@@ -14,6 +14,7 @@ export default function AgendaCita() {
                 <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
                 <h2 className="title-font font-medium text-3xl text-gray-900" id='agendacitaahora'>¿Listo para dar el primer paso hacia tu bienestar? Estamos aquí para ti. </h2>
                 <p className="leading-relaxed mt-4">Agenda tu cita ahora y comencemos juntos este viaje hacia una vida más saludable y equilibrada.</p>
+                <p className="leading-relaxed mt-4">Te brindaremos un  un espacio seguro para hablar sobre tus pensamientos y sentimientos. Ayuda a identificar y comprender patrones de pensamiento negativos o comportamientos destructivos, lo que puede reducir la ansiedad, la depresión y otros trastornos mentales</p>
                 </div>
                 <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
                 <DoctoraliaWidget/>
---
+++ pages/Taller/Taller-reconecta-con-tu-energia-femenina.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TallerReconectaconTuEnergiaFemenina() {
+  return (
+    <div>Taller-reconecta-con-tu-energia-femenina</div>
+  )
+}
