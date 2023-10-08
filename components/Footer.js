ab8fc02dfb6a39f43744d825a4ab4357e6e2fe28
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-10-07 18:40:30 -0700

    add target blank on link footer rrss

--- components/Footer.js
+++ components/Footer.js
@@ -121,14 +121,14 @@ export default function Footer() {
                     </p>
                     {/* RED SOCIAL FB */}
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                    <Link href="https://www.facebook.com/UneteDesarrolloIntegral" className="text-gray-500 hover:text-pink-800">
+                    <Link href="https://www.facebook.com/UneteDesarrolloIntegral" className="text-gray-500 hover:text-pink-800" target="_blank">
                             <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                             </svg>
                     </Link>
                         {/* RED SOCIAL INSTAGRAM */}
                         
-                    <Link href="https://www.instagram.com/unetedesarrollointegral/" className="ml-3 text-gray-500 hover:text-pink-800">
+                    <Link href="https://www.instagram.com/unetedesarrollointegral/" className="ml-3 text-gray-500 hover:text-pink-800" target="_blank">
                             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                 <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
