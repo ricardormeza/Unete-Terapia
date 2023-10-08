1ed4dbf2e6e9c20035470d855d3ee23fb316b04d
RickyRaMx_c149 <ricardo.rmeza@live.com>
2023-10-07 22:06:55 -0700

    add whatsapp link a talleres cta

--- pages/Taller/Taller-reconecta-con-tu-energia-femenina.jsx
+++ pages/Taller/Taller-reconecta-con-tu-energia-femenina.jsx
@@ -173,7 +173,9 @@ export default function TallerReconectaconTuEnergiaFemenina() {
                         <div className="container px-5 py-24 mx-auto">
                             <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
                             <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">Agenda tu cita ahora y comencemos juntos este viaje hacia una vida más saludable y equilibrada.</h1>
-                            <button className="flex-shrink-0 text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-400 rounded text-lg mt-10 sm:mt-0">Escribeme</button>
+                            <Link href="https://wa.link/x0p32b"  legacyBehavior target="_blank" >
+                                <button className="flex-shrink-0 text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-400 rounded text-lg mt-10 sm:mt-0">Escribeme</button>
+                            </Link>
                             </div>
                         </div>
                     </section>
